79025941831a2031485007f7f440319c15c364a2
Ani Sridhar <asridha1@andrew.cmu.edu>
2015-07-30 16:48:37 -0700

    testing hash.js

--- traditional/hash.js
+++ traditional/hash.js
@@ -1,9 +1,7 @@
-console.log("The time is 3:56 pm.\n");
 
 $(document).on('login',"#login",
         function(event){
 
-            console.log("I hope this doesn't break!!!\n");
 
         }
     );

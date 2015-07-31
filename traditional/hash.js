5410e4de44785aaee8e27a41e060f9823dd0503a
Ani Sridhar <asridha1@andrew.cmu.edu>
2015-07-31 15:20:07 -0700

    trying to fix dummy

--- traditional/hash.js
+++ traditional/hash.js
@@ -2,6 +2,10 @@ var hash = (function(){
 
     var module = {};
 
+    function dummy(){
+        console.log("hellooo\n");
+    }
+
     //controller methods
     module.dummy = function (){
         console.log("Ran dummy\n");

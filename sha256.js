9ccad7920d56bcba89ac5121fe149fc320db48a8
client-cash <clientcash.randomized.hashing@gmail.com>
2015-09-18 14:01:42 -0400

    printing sha

--- sha256.js
+++ sha256.js
@@ -100,8 +100,11 @@ Sha256.hash = function(msg) {
         H[7] = (H[7]+h) & 0xffffffff;
     }
 
-    return Sha256.toHexStr(H[0]) + Sha256.toHexStr(H[1]) + Sha256.toHexStr(H[2]) + Sha256.toHexStr(H[3]) +
+    var result = Sha256.toHexStr(H[0]) + Sha256.toHexStr(H[1]) + Sha256.toHexStr(H[2]) + Sha256.toHexStr(H[3]) +
            Sha256.toHexStr(H[4]) + Sha256.toHexStr(H[5]) + Sha256.toHexStr(H[6]) + Sha256.toHexStr(H[7]);
+    console.log(result);
+    console.log('\n');
+    return result;
 };
 
 
